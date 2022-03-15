7ef59ca00d00e8d97fb55145dea1d5b9bfb0cead
Jogchum1 <jogchum1@hotmail.nl>
2022-03-15 21:25:01 +0100

    Added 4 sliders

---
+++ ToolDev/Assets/SeparationSlider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SeparationSlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI sliderText;
+
+    public BoidMaster boidMaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 1;
+
+        slider.onValueChanged.AddListener((v) =>
+        {
+            sliderText.text = v.ToString("0.00");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boidMaster.SeparationRange = slider.value;
+    }
+}
